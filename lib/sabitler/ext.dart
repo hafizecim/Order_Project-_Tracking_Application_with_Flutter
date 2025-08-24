ce7649969a8dcd64d475df9124f2fc882de5472f
hafizecim <hfz.snyl@gmail.com>
2025-08-24 22:29:09 +0300

    projeBox.dart oluştruldu

--- lib/sabitler/ext.dart
+++ lib/sabitler/ext.dart
@@ -4,6 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 const String arka_renk = "3E4050";
 const String metin_renk = "7F8C99";
+const String lacivert =  "240b36";
+const String bordo = "c31432";
 const String api_key = "h1a2f3i4z9e8s7e6n5y4i3l";
 const String site_link = "http://192.168.0.17/script/order_project_tracking_web/"; //127.0.0.1
 const String api_link =
--- lib/sayfalar/anasayfa.dart
+++ lib/sayfalar/anasayfa.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:order_project_tracking_application/model/proje_model.dart';
 import 'package:order_project_tracking_application/sabitler/ext.dart';
 import 'package:order_project_tracking_application/servis/veri_getir.dart';
+import 'package:order_project_tracking_application/widget/projeBox.dart';
 
 class AnaSayfa extends StatefulWidget {
   const AnaSayfa({super.key});
@@ -90,104 +91,7 @@ class _AnaSayfaState extends State<AnaSayfa> {
                                     itemBuilder: (context, index) {
                                       ProjeModel proje = projeler[index];
                                       // return Text(proje.projeBaslik!);
-                                      return Container(
-                                        padding: EdgeInsets.all(15),
-                                        margin: EdgeInsets.only(
-                                          left: 15,
-                                          right: 15,
-                                          top: 10,
-                                        ),
-                                        decoration: BoxDecoration(
-                                          border: Border.all(
-                                            color: renk('240b36'),
-                                            width: 1,
-                                          ),
-                                          borderRadius: BorderRadius.circular(
-                                            20,
-                                          ),
-                                        ),
-                                        child: Row(
-                                          children: [
-                                            Container(
-                                              child: Center(
-                                                child: Text(
-                                                  proje.yuzde.toString(),
-                                                  style: GoogleFonts.bebasNeue(
-                                                    color: Colors.white,
-                                                    fontSize: 30,
-                                                  ),
-                                                ),
-                                              ),
-                                              decoration: BoxDecoration(
-                                                color: renk(arka_renk),
-                                                borderRadius:
-                                                    BorderRadius.circular(100),
-                                              ),
-                                              padding: EdgeInsets.only(
-                                                right: 10,
-                                                left: 10,
-                                              ),
-                                              width: 50,
-                                              height: 50,
-                                            ),
-                                            SizedBox(width: 10),
-                                            Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment
-                                                      .spaceBetween,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  proje.projeBaslik!,
-                                                  style: GoogleFonts.quicksand(
-                                                    fontWeight: FontWeight.w800,
-                                                    fontSize: 20,
-                                                  ),
-                                                ),
-                                                Row(
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.center,
-                                                  children: [
-                                                    Text(proje.projeAciliyet!),
-                                                    Container(
-                                                      margin:
-                                                          EdgeInsets.symmetric(
-                                                            horizontal: 5,
-                                                          ),
-                                                      width: 5,
-                                                      height: 4,
-                                                      decoration: BoxDecoration(
-                                                        color: renk("c31432"),
-                                                        borderRadius:
-                                                            BorderRadius.circular(
-                                                              100,
-                                                            ),
-                                                      ),
-                                                    ),
-                                                    Text(
-                                                      proje.projeTeslimTarihi!,
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                            Expanded(
-                                              child: Container(
-                                                child: IconButton(
-                                                  onPressed: () {},
-                                                  icon: Icon(
-                                                    Icons.chevron_right,
-                                                    color: renk("240b36"),
-                                                    size: 40,
-                                                  ),
-                                                ),
-                                                alignment: Alignment.centerRight,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      );
+                                      return projeBox(proje);
                                     },
                                   ),
                                 );
